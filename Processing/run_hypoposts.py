e274f54ed93a7483567b7aedb25d726271dbd204
MaLuHart <38753077+MaLuHart@users.noreply.github.com>
2018-10-12 15:18:19 +0200

    Schoech Routine

---
+++ Processing/run_hypoposts.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# Filename: run_hypoposts.py
+# Author: Christof Schoech
+
+
+
+"""
+# Function to build corpora from hypotheses.org blog data.
+# Bulk download, metadata extraction, plain text extraction.
+#
+# This is the parameter and control script.
+"""
+
+
+import hypoposts
+from os.path import join
+
+
+# ========================================
+# Parameters
+# ========================================
+
+
+wdir = "/media/christof/data/Dropbox/0-Analysen/2017/hypoposts/all/" 
+urlfile = join(wdir, "urls-missing.txt")
+htmlfolder = join(wdir, "html", "")
+txtfolder = join(wdir, "txt", "")
+minlength = 50
+metadatafile =  join(wdir, "metadata.csv")
+collfolder = join(wdir, "coll", "")
+language = "de"
+numposts = 3
+textlengths = [4000, 3000, 2000, 1500, 1000, 500, 250, 125]
+variability = 0.1
+
+
+# ========================================
+# Functions 
+# ========================================
+
+
+# hypoposts.get_hypoposts(urlfile, htmlfolder)
+# hypoposts.extract_data(htmlfolder, txtfolder, minlength)
+# hypoposts.analyze_metadata(metadatafile)
+hypoposts.build_collection(metadatafile, language, numposts, textlengths, variability)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
